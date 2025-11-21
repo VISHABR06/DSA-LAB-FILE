7341c89172d46cbfd6d1b54f194ffe10f31a1301
VISHABR06 <vishusingh09888@gmail.com>
2025-11-22 00:25:55 +0530

    program 17

---
+++ practical17.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Create new node
+Node* createNode(int x) {
+    Node* temp = new Node();
+    temp->data = x;
+    temp->left = temp->right = NULL;
+    return temp;
+}
+
+// Preorder: Root → Left → Right
+void preorder(Node* root) {
+    if (root == NULL) return;
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+// Inorder: Left → Root → Right
+void inorder(Node* root) {
+    if (root == NULL) return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+// Postorder: Left → Right → Root
+void postorder(Node* root) {
+    if (root == NULL) return;
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main() {
+    // Creating tree manually
+    Node* root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+
+    cout << "Preorder: ";
+    preorder(root);
+
+    cout << "\nInorder: ";
+    inorder(root);
+
+    cout << "\nPostorder: ";
+    postorder(root);
+
+    return 0;
+}
